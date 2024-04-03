2b21030887ff91b12be6554f0d10b08c8f941dc6
ozgur <ozgurozkankar@gmail.com>
2024-04-03 15:56:22 +0300

    eslint added, simple dummy api file created

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'react-app',
+      'react-app/jest',
+    ],
+    rules: {
+      // Additional rules or overrides can go here
+    },
+  };
+  
